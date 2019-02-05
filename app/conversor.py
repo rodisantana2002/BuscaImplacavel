20d46c4967180b45b74a114a744f9b5fd6755b14
Rodolfo Santana <rodisantana2002@gmail.com>
2019-02-05 16:19:21 -0200

    gfhf

--- app/conversor.py
+++ app/conversor.py
@@ -43,44 +43,193 @@ class conversor(object):
                             'msgRetorno']
 
     def gerarFileBase(self):
+        strNone='------------------------------------------------------------'
+        with open(self.destino, 'w+') as source:
+            writer = csv.DictWriter(source, fieldnames=self.FIELD_NAMES)
+            writer.writeheader()
+
         for arquivo in self.files_path(self.origem):
-            with open(self.destino, 'w+') as source:
-                writer = csv.DictWriter(source, fieldnames=self.FIELD_NAMES)
-                writer.writeheader()
-                with open(arquivo, 'r') as base:
-                    reader = csv.DictReader(base)
-                    logger.debug('+--------------------------------------------------+')
-                    logger.debug('|     Inciando script de conversão de arquivos     |')
-                    logger.debug('+--------------------------------------------------+')
-                    count = 1
-                    for row in reader:
-                        if count <= self.limite:
-                            writer.writerow({'id':row['Identifier'],
-                                             'title':row['Title'],
-                                             'year':row['Year'],
-                                             'author':row['Author'],
-                                             'doi':row['DOI'],
-                                             'url':row['URL'],
-                                             'keywords':row['Custom3'],
-                                             'tipo':row['Publisher'],
-                                             'base':row['BibliographyType'],
-                                             'situacao':'pendente',
-                                             'numTentativas':'none',
-                                             'possuiCaptcha':'none',
-                                             'valorCaptcha':'none',
-                                             'msgRetorno':''
-                                            })
+            if arquivo.lower().__contains__('jabref'):
+                self._carregarJabref(arquivo)
 
-                            strLog = '---> {0} - {1}'.format(row['Identifier'], row['DOI'])
-                            logger.debug(strLog)
-                        count+=1
+            elif arquivo.lower().__contains__('ieee'):
+                self._carregarIEEE(arquivo)
 
-        logger.debug ('----------------------------------------------------------')
+            elif arquivo.lower().__contains__('springer'):
+                self._carregarSpringer(arquivo)
+
+            elif arquivo.lower().__contains__('acm'):
+                self._carregarACM(arquivo)
+
+            elif arquivo.lower().__contains__('sciencedirect'):
+                self._carregarScience(arquivo)
+
+            else:
+                strNone='------------------------------------------------------------'
+
+
+        logger.debug (strNone)
         logger.debug ('---> Finalizando preparação do arquivo base.')
 
     def files_path(self, path):
         '''return list of string'''
         return ([path+file for p, _, files in os.walk(os.path.abspath(path)) for file in files if file.lower().endswith(".csv")])
+
+    def _carregarJabref(self, arquivo):
+        with open(self.destino, 'a+') as source:
+            writer = csv.DictWriter(source, fieldnames=self.FIELD_NAMES)
+            with open(arquivo, 'r') as base:
+                reader = csv.DictReader(base)
+                logger.debug('+----------------------------------------------------------+')
+                logger.debug('|     Inciando script de conversão de arquivos (JabRef)    |')
+                logger.debug('+----------------------------------------------------------+')
+                count = 1
+                for row in reader:
+                    if count <= self.limite:
+                        writer.writerow({'id':row['Identifier'],
+                                         'title':row['Title'],
+                                         'year':row['Year'],
+                                         'author':row['Author'],
+                                         'doi':row['DOI'],
+                                         'url':row['URL'],
+                                         'keywords':row['Custom3'],
+                                         'tipo':row['Publisher'],
+                                         'base':row['BibliographyType'],
+                                         'situacao':'pendente',
+                                         'numTentativas':'none',
+                                         'possuiCaptcha':'none',
+                                         'valorCaptcha':'none',
+                                         'msgRetorno':''
+                                        })
+
+                        strLog = '---> {0} - {1}'.format(row['Identifier'], row['DOI'])
+                        logger.debug(strLog)
+                    count+=1
+
+    def _carregarIEEE(self, arquivo):
+        with open(self.destino, 'a+') as source:
+            writer = csv.DictWriter(source, fieldnames=self.FIELD_NAMES)
+            with open(arquivo, 'r') as base:
+                reader = csv.DictReader(base)
+                logger.debug('+----------------------------------------------------------+')
+                logger.debug('|      Inciando script de conversão de arquivos (IEEE)     |')
+                logger.debug('+----------------------------------------------------------+')
+                count = 1
+                for row in reader:
+                    if count <= self.limite:
+                        writer.writerow({'id': 'IEEE-{0}-{1}'.format(row['Publication_Year'], count),
+                                         'title':row['Document Title'],
+                                         'year':row['Publication_Year'],
+                                         'author':row['Authors'],
+                                         'doi':row['DOI'],
+                                         'url':row['PDF Link'],
+                                         'keywords':'none',
+                                         'tipo':row['Publisher'],
+                                         'base':row['Document Identifier'],
+                                         'situacao':'pendente',
+                                         'numTentativas':'none',
+                                         'possuiCaptcha':'none',
+                                         'valorCaptcha':'none',
+                                         'msgRetorno':''
+                                        })
+
+                        strLog = '---> {0} - {1}'.format(row['Publication_Year'], row['DOI'])
+                        logger.debug(strLog)
+                    count+=1
+
+    def _carregarSpringer(self, arquivo):
+        with open(self.destino, 'a+') as source:
+            writer = csv.DictWriter(source, fieldnames=self.FIELD_NAMES)
+            with open(arquivo, 'r') as base:
+                reader = csv.DictReader(base)
+                logger.debug('+----------------------------------------------------------+')
+                logger.debug('|    Inciando script de conversão de arquivos (Springer)   |')
+                logger.debug('+----------------------------------------------------------+')
+                count = 1
+                for row in reader:
+                    if count <= self.limite:
+                        writer.writerow({'id':'Springer-{0}-{1}'.format(row['Publication Year'], count),
+                                         'title':row['Item Title'],
+                                         'year':row['Publication Year'],
+                                         'author':row['Authors'],
+                                         'doi':row['Item DOI'],
+                                         'url':row['URL'],
+                                         'keywords':'none',
+                                         'tipo':'Springer',
+                                         'base':row['Content Type'],
+                                         'situacao':'pendente',
+                                         'numTentativas':'none',
+                                         'possuiCaptcha':'none',
+                                         'valorCaptcha':'none',
+                                         'msgRetorno':''
+                                        })
+
+                        strLog = '---> {0} - {1}'.format(row['Publication Year'], row['Item DOI'])
+                        logger.debug(strLog)
+                    count+=1
+
+    def _carregarACM(self, arquivo):
+        with open(self.destino, 'a+') as source:
+            writer = csv.DictWriter(source, fieldnames=self.FIELD_NAMES)
+            with open(arquivo, 'r') as base:
+                reader = csv.DictReader(base)
+                logger.debug('+----------------------------------------------------------+')
+                logger.debug('|     Inciando script de conversão de arquivos (ACM)       |')
+                logger.debug('+----------------------------------------------------------+')
+                count = 1
+                for row in reader:
+                    if count <= self.limite:
+                        writer.writerow({'id':'ACM-{0}-{1}'.format(row['id'], row['year']),
+                                         'title':row['title'],
+                                         'year':row['year'],
+                                         'author':row['author'],
+                                         'doi':row['doi'],
+                                         'url':'none',
+                                         'keywords':'none',
+                                         'tipo':row['publisher'],
+                                         'base':row['type'],
+                                         'situacao':'pendente',
+                                         'numTentativas':'none',
+                                         'possuiCaptcha':'none',
+                                         'valorCaptcha':'none',
+                                         'msgRetorno':''
+                                        })
+
+                        strLog = '---> {0} - {1}'.format(row['id'], row['doi'])
+                        logger.debug(strLog)
+                    count+=1
+
+    def _carregarScience(self, arquivo):
+        with open(self.destino, 'a+') as source:
+            writer = csv.DictWriter(source, fieldnames=self.FIELD_NAMES)
+            writer.writeheader()
+            with open(arquivo, 'r') as base:
+                reader = csv.DictReader(base)
+                logger.debug('+----------------------------------------------------------+')
+                logger.debug('|    Inciando script de conversão de arquivos (Science)    |')
+                logger.debug('+----------------------------------------------------------+')
+                count = 1
+                for row in reader:
+                    if count <= self.limite:
+                        writer.writerow({'id':row['Identifier'],
+                                         'title':row['Title'],
+                                         'year':row['Year'],
+                                         'author':row['Author'],
+                                         'doi':row['DOI'],
+                                         'url':row['URL'],
+                                         'keywords':'none',
+                                         'tipo':row['Publisher'],
+                                         'base':row['BibliographyType'],
+                                         'situacao':'pendente',
+                                         'numTentativas':'none',
+                                         'possuiCaptcha':'none',
+                                         'valorCaptcha':'none',
+                                         'msgRetorno':''
+                                        })
+
+                        strLog = '---> {0} - {1}'.format(row['Identifier'], row['DOI'])
+                        logger.debug(strLog)
+                    count+=1
 
 # carrega script
 def main():
