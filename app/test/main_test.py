40474ede6b5f3c2d9b334361953b75203977ce28
rodolfo <osboxes@linux.localdomain>
2020-04-17 16:42:38 -0300

    asd

--- app/test/main_test.py
+++ app/test/main_test.py
@@ -1,10 +1,32 @@
 import bibtexparser
 from sqlalchemy import create_engine
+import PySimpleGUI as sg
+
+
+class tela(object):
+
+    def __init__(self):
+        layout = [
+            [sg.Text('Nome'), sg.Input()],
+            [sg.Button('enviar')],
+            [sg.Ok('')]
+        ]
+        janela = sg.Window("Dados").layout(layout)
+        self.button, self.values = janela.Read()
+
+    def Iniciar(self):
+        print(self.values)
 
 def main():
-    with open('/home/rodolfosantana/Documentos/projetos/automator/buscaimplacavel/bases/referencias/JABREF-2015-46.BibText') as bibtex_file:
-        bib_database = bibtexparser.load(bibtex_file)
-    print(bib_database.entries[0]['title'])
+
+    layout  = [[sg.Text(f'{i}. '), sg.In(key=i)] for i in range(1,6)] + [[sg.Button('Save'), sg.Button('Exit')]]
+
+    window = sg.Window('To Do List Example', layout)
+
+    event, values = window.read()
+    # with open('/home/rodolfosantana/Documentos/projetos/automator/buscaimplacavel/bases/referencias/JABREF-2015-46.BibText') as bibtex_file:
+    #     bib_database = bibtexparser.load(bibtex_file)
+    # print(bib_database.entries[0]['title'])
 
     # engine = create_engine('sqlite:///../buscaimplacavel/bases/database/bot.db')
 
